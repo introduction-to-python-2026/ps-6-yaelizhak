c9e9fb04dbaab30b56d6411a66e9a6af09e33e30
YaelIzhak <yaelizhak2001@gmail.com>
2025-11-27 16:54:35 +0200

    Update codons.py
    4

--- codons.py
+++ codons.py
@@ -1,15 +1,15 @@
-from data import codon.txt as path
+
 def create_codon_dict(path):
     codon_to_amino_acid = {}
     with open(path, 'r') as f:
-    for row in f:
-        cell = row.strip().split('\t')
-        if len(cells) < 3:
-                continue
-        codon= cell[0]
-        acid = cell[2]
-        codon_to_amino_acid [codon] = acid
-    
+        for row in f:
+            cell = row.strip().split('\t')
+            if len(cell) < 3:
+                    continue
+            codon= cell[0]
+            acid = cell[2]
+            codon_to_amino_acid [codon] = acid
+        
     return codon_to_amino_acid
 
 
